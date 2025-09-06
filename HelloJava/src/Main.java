f076973304ce8c6551e26c89df0a6bab456035d1
s202255570 <yyouseong@gmail.com>
2025-09-07 00:55:47 +0900

    second commit

--- HelloJava/src/Main.java
+++ HelloJava/src/Main.java
@@ -1,6 +1,7 @@
-import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//import java.util.Scanner;
 public class Main {
 
     public void foo() {
