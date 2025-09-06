e2bf4ac981e83fd45ec8d56c1591577f9841c4f9
s202255570 <yyouseong@gmail.com>
2025-09-06 23:47:41 +0900

    first commit

---
+++ HelloJava/src/HelloJava.java
@@ -0,0 +1,6 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        // 이 코드는 콘솔에 "Hello, Java World!"라고 출력합니다.
+        System.out.println("Hello, Java World!");
+    }
+}
