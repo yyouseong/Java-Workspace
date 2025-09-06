658c6eaf96632b39d02024c6639d318034da53a9
s202255570 <yyouseong@gmail.com>
2025-09-07 00:53:00 +0900

    second commit

--- HelloJava/src/HelloJava.java
+++
@@ -1,6 +0,0 @@
-public class HelloJava {
-    public static void main(String[] args) {
-        // 이 코드는 콘솔에 "Hello, Java World!"라고 출력합니다.
-        System.out.println("Hello, Java World!");
-    }
-}
--- HelloJava/src/Main.java
+++ HelloJava/src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    public void foo() {
+        System.out.println("Fooo!!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello Java.");
+        System.out.println("Hello java" + " !!" + 12.1234);
+        // foo(); // Not a good way sometimes
+        Main myMain = new Main();
+        myMain.foo();
+        System.out.println("myMain: " + myMain);
+        //Create a Scanner object
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        Double number1 = input.nextDouble();
+        System.out.println("number1: " + number1);
+        */
+        System.out.println(1/2);
+        System.out.println((double)1/2);
+        System.out.println((double)(1/2));
     }
 }
